4ad66562cc81e5c5c57a7ffcfad8dfe4ce3a0a70
artemidza1848 <venerocka80@gmail.com>
2025-01-22 16:48:45 +0300

    Более нормальнее сделал переменные и добавил 4 функции для +, -, * и /

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,16 @@
+def plus(number1, number2):
+    print(number1 + number2)
 
+def minus(number1, number2):
+    print(number1 - number2)
+
+def multiply(number1, number2):
+    print(number1 * number2)
+
+def devision(number1, number2):
+    print(number1 / number2)
+
+number1 = int(input("Введите число: "))
 number2 = int(input("Введите число: "))
 access = input("Выберите +, -, * или /")
 
