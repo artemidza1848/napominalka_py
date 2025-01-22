fb0a0a96c48ea243564ea70d6a92fb05be1f07e4
artemidza1848 <venerocka80@gmail.com>
2025-01-22 16:46:08 +0300

    Добавил 2 переменные для числа и оператора. 💀

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,4 @@
-def plus(number1, number2):
-    print(number1 + number2)
 
-def minus(number1, number2):
-    print(number1 - number2)
-
-def multiply(number1, number2):
-    print(number1 * number2)
-
-number1 = int(input("Введите число: "))
 number2 = int(input("Введите число: "))
 access = input("Выберите +, -, * или /")
 
